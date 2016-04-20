8f3f4e59ac7e3700f19a0e1f0d8c8f306d88fc4f
priya kumari <priya kumari>
2016-04-20 14:09:29 +0530

    working with mobile no

---
+++ AndroidLoginAndRegistration/app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
@@ -0,0 +1,9 @@
+package info.androidhive.loginandregistration.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://172.16.17.38/blood/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://172.16.17.38/blood/register.php";
+}
